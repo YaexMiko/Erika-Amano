9a66e26283b0e10f87a9814b7fb5663e267327ea
Yae Miko <aloksinghmains756@gmail.com>
2025-05-26 17:05:22 +0530

    Update encoder.py
    Signed-off-by: Yae Miko  <aloksinghmains756@gmail.com>

--- Bot/plugins/encoder.py
+++ Bot/plugins/encoder.py
@@ -3,7 +3,7 @@ import time, os
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup as IKM, InlineKeyboardButton as IKB
 
-from Bot.plugins.database.mongo_db import check_user_mdb,check_vcodec_settings,update_vcodec_settings,check_preset_settings,update_preset_settings,check_resolution_settings,check_audio_type_mdb, update_audio_type_mdb,update_resolution_settings
+from Bot.plugins.database.mongo_db import ensure_user_exists, check_vcodec_settings, check_preset_settings, check_resolution_settings, check_audio_type_mdb
 from Bot.utils.decorators import ffmpeg_settings
 from Bot import encoder, OWNER_ID, LOG, FILES_CHANNEL, SESSION_STRING , ubot
 from Bot.utils.progress_pyro import progress_for_pyrogram
@@ -12,7 +12,6 @@ from Bot.utils.ffmpeg import ffmpeg_progress
 encoder_is_on = []
 flood = []
 
-    
 async def on_task_complete():
     del encoder_is_on[0]
     if len(encoder_is_on) > 0:
@@ -32,7 +31,7 @@ async def add_task(message):
             try:
                 await ffmpeg_progress(cmd, filepath,f'progress-{FT}.txt',FT, msg, '**Encoding Started**\n\n')
             except Exception as e:
-                LOG.info(f'ERror while ffmpeg progress\n' +e)  
+                LOG.info(f'Error while ffmpeg progress\n{str(e)}')  
             output = filepath.rsplit('.',1)[0]
             if '.mkv' in filepath:
                 output = output+'_IA.mkv'    
@@ -41,25 +40,25 @@ async def add_task(message):
                 
             file_name = output.rsplit('/', 1)[1].replace('_IA', "")
             try: #MSG EDIT AND EDIT
-                await msg.edit(f'**Encoding Completed')   
+                await msg.edit(f'**Encoding Completed**')   
                 if ubot == None:
                     file =  await encoder.send_document(chat_id = FILES_CHANNEL, document = output, caption=f"**{check_resolution}**", file_name=file_name)  
                 else:
                     file =  await ubot.send_document(chat_id = FILES_CHANNEL, document = output, caption=f"**{check_resolution}**", file_name=file_name)  
                 
             except Exception as e: 
-                LOG.info(f'Error while file sending\n'+e)  
+                LOG.info(f'Error while file sending\n{str(e)}')  
             try:
                 await encoder.copy_message(chat_id = msg.chat.id, from_chat_id = FILES_CHANNEL, message_id= file.id)                
             except Exception as e: 
-                LOG.info(f'Error while file copy\n'+e)
+                LOG.info(f'Error while file copy\n{str(e)}')
                 
             try: #FILE DELETE
                 os.remove(filepath)
                 os.remove(output)
                 os.remove(f'progress-{FT}.txt')
             except Exception as e: 
-                LOG.info(f'Error while removing files\n'+e)      
+                LOG.info(f'Error while removing files\n{str(e)}')      
            
             try: #MSG DELETE
                 await msg.delete() 
@@ -67,17 +66,14 @@ async def add_task(message):
                 pass       
                        
         except Exception as e: 
-            LOG.info(f'Error while line 56\n'+e)    
+            LOG.info(f'Error while downloading/encoding\n{str(e)}')    
     except Exception as e: 
-        LOG.info(f'Error while Line 58\n'+e)
+        LOG.info(f'Error in add_task\n{str(e)}')
         
     try:
         await on_task_complete()   
     except Exception as e: 
-       # await on_task_complete()   
-        LOG.info(f'Error while task complete\n'+e) 
-       
-
+        LOG.info(f'Error while task complete\n{str(e)}') 
 
 video_mimetype = [
     "video/x-flv",
@@ -97,22 +93,39 @@ video_mimetype = [
 ]      
                   
 @encoder.on_message(filters.document | filters.video & filters.private)
-async def encoder_process(encoder, message):
+async def encoder_process(client, message):
     if message.document and message.video and message.document.mime_type and message.video.mime_type not in video_mimetype:
         return 
-    check = check_user_mdb(message.from_user.id)
-    if check is None:
-        text= "You're not authorized to use this bot. Request Admins to approve you."
-        await encoder.send_message(message.chat.id, text, reply_markup=IKM([[IKB('ʀᴇǫᴜᴇsᴛ', f'users_request-{message.from_user.id}')]]))
-        return
-    msf = await message.reply('Added to the queue')
+    
+    # Auto-create user settings if they don't exist (removes approval requirement)
+    ensure_user_exists(message.from_user.id)
+    
+    msf = await message.reply('📥 Added to the queue')
     encoder_is_on.append(message)
     if len(encoder_is_on) == 1:
         await msf.delete()
         await add_task(message)           
-                         
-                   
-        
-        
-        
-                                
+
+@encoder.on_message(filters.command(['start', 'help'], prefixes=['/']))
+async def start_handler(client, message):
+    # Auto-create user settings on first interaction
+    ensure_user_exists(message.from_user.id)
+    
+    text = f'**Hi There** `{message.from_user.first_name}`,\n\nI am a video encoder bot, which reduces the size of the video and gives it in good quality.\n\n✅ **No approval required** - You can start using the bot immediately!\n\nTo see all my features, click the buttons below'
+    
+    buttons = IKM([
+        [
+            IKB("ᴅᴇᴠᴇʟᴏᴘᴇʀ", 'answer_about_dev'),
+        ], 
+        [
+            IKB('ʀᴇsᴏʟᴜᴛɪᴏɴ', 'answer_resolution'),
+            IKB('ᴀᴜᴅɪᴏ', 'answer_audio'),
+            IKB('ᴄʀғ', 'answer_crf')
+        ],
+        [
+           IKB('ᴠᴄᴏᴅᴇᴄ', 'answer_vcodec'),
+           IKB('ᴘʀᴇsᴇᴛ', 'answer_preset'), 
+        ]
+    ])
+    
+    await message.reply(text, reply_markup=buttons)
